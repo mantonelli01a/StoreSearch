e785526a2da62964936c213ffdbc407c2ceb0fbc
mantonelli01a <mike@mantonelli.plus.com>
2017-07-01 20:40:17 +0100

    p660 up to improving the data model

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableview: UITableView!
+    
+    var searchResults: [String] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableview.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +29,48 @@ class SearchViewController: UIViewController {
 
 }
 
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        searchResults = []
+        for i in 0...2 {
+            searchResults.append(String(format: "Fake Result %d for '%@'", i, searchBar.text!))
+        }
+        tableview.reloadData()
+    }
+    
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
+    }
+}
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "SearchResultCell"
+        
+        var cell: UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
+        }
+        /*
+         if searchResults.count == 0 {
+         cell.textLabel!.text = "(Nothing found)"
+         cell.detailTextLabel!.text = ""
+         } else {
+         let searchResult = searchResults[indexPath.row]
+         cell.textLabel!.text = searchResult.name
+         cell.detailTextLabel!.text = searchResult.artistName
+         }
+         */
+        cell.textLabel!.text = searchResults[indexPath.row]
+        return cell
+    }
+}
+
+extension SearchViewController: UITableViewDelegate {
+    
+}
